da2b7f73f15949593200514f120bec630b3ceee8
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-10-21 13:17:14 +0200

    save

--- commit_and_upload.sh
+++ commit_and_upload.sh
@@ -4,7 +4,7 @@ files_to_upload=`git ls-files --others --modified`
 for file_to_upload in $files_to_upload
 do
 	git add $file_to_upload
-	#curl ftp...
+	curl --ftp-create-dirs -T file_to_upload -u mammusique@mammusique.eu:$ftp_password ftp://ftp.mammusique.eu
 done
 
 git commit -m "save"
